dba42b12f7c9b56d0707ab343ee17240eb56a41a
wandesky <bwandesky@gmail.com>
2018-10-12 08:59:45 +0300

    [finishes #161143951] create view products page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 
 var modal = document.getElementById('myModal');
+var updateModal = document.getElementById('updateModal');
+var updateBtn = document.getElementById("update");
 var btn = document.getElementById("myBtn");
 var btn1 = document.getElementById("myBtn1");
 var btn2 = document.getElementById("myBtn2");
@@ -7,6 +9,10 @@ var btn3 = document.getElementById("myBtn3");
 var btn4 = document.getElementById("myBtn4");
 var btn5 = document.getElementById("myBtn5");
 var span = document.getElementsByClassName("close")[0];
+
+updateBtn.onclick = function(){
+    updateModal.style.display = "block";
+}
 
 btn.onclick = function(){
     modal.style.display = "block";
@@ -38,7 +44,7 @@ span.onclick = function(){
 
 window.onclick = function(event){
     if(event.target == modal){
-        modaly.style.display = "none";
+        modal.style.display = "none";
     }
 }
 
