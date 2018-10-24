de4edc68e0b6e10b394fa689fdd22702f600c6a7
wandesky <bwandesky@gmail.com>
2018-10-24 08:53:37 +0300

    [finishes #161220556] write failing tests

---
+++ app/api/v1/views/sales.py
@@ -0,0 +1,91 @@
+from flask import Flask
+from flask_restplus import Api, Resource, fields
+from werkzeug.contrib.fixers import ProxyFix
+
+
+app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
+api = Api(app, version='1', title='Store Manager API',
+    description='Backend code for the Andela Store Manager Challenge',
+    )
+
+sales_ns = api.namespace('sales', description='Sale operations')
+
+sale = api.model('Sale', {
+    'sale-id':fields.Integer(readOnly = True, description='The sale unique identifier'),
+    'product-id':fields.Integer(required=True, description='The id of the item sold'),
+    'attendant':fields.String(required=True, description='The attendant who made the sale')
+})
+
+class SaleDataAccessObject(object):
+    def __init__(self):
+        self.counter = 0
+        self.sales = []
+
+    def get(self, id):
+        for sale in self.sales:
+            if sale['id'] == id:
+                return sale
+            api.abort(404, "Sale record {} does not exist".format(id))
+
+    def create(self, data):
+        sale = data
+        sale['id'] = self.counter = self.counter + 1
+        self.sales.append(sale)
+        return sale
+
+    def update(self, id, data):
+        sale = self.get(id)
+        sale.update(data)
+        return sale
+
+    def delete(self, id):
+        sale = self.get(id)
+        self.sales.remove(sale)
+
+DemoSale = SaleDataAccessObject()
+DemoSale.create({'sale':'Spoon'})
+DemoSale.create({'sale':'Cup'})
+DemoSale.create({'sale':'Jug'})
+
+@sales_ns.route('/')
+class Sales(Resource):
+    '''Displays a list of all sales with provision to POST a new sale'''
+    @sales_ns.doc('list_sales')
+    def get(self):
+        '''List all sales records'''
+        return DemoSale.sales
+    
+    @sales_ns.doc('create a_sale')
+    @sales_ns.expect(sale)
+    @sales_ns.marshal_with(sale, code=201)
+    def post(self):
+        '''Create a new sale record'''
+        return DemoSale.create(api.payload), 201
+
+@sales_ns.route('/<int:id>')
+@sales_ns.response(404, 'Sale record not found not found')
+@sales_ns.param('id', 'Unique identifier of the sold item')
+class Sale(Resource):
+    '''Show a single sale with provision for deleting existing sales'''
+    @sales_ns.doc('get_sale')
+    @sales_ns.marshal_with(sale)
+    def get(self, id):
+        '''Fetch a given resource'''
+        return DemoSale.get(id)
+
+    @sales_ns.doc('delete_sale')
+    @sales_ns.response(204, 'Sale deleted')
+    def delete(self, id):
+        '''Delete a sale using its identifier'''
+        DemoSale.delete(id)
+        return '', 204
+
+    @sales_ns.expect(sale)
+    @sales_ns.marshal_with(sale)
+    def put(self, id):
+        '''Update a sale using its identifier'''
+        return DemoSale.update(id, api.payload)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ app/tests/v1/test_sales.py
@@ -0,0 +1,53 @@
+import unittest 
+import json
+from app import create_app
+from instance.config import app_config
+
+
+class TestSale(unittest.TestCase):
+
+    def setUp(self):
+        # self.app = create_app(config_name="testing")
+        self.app = create_app(config_name="testing")
+        self.app.testing = True
+        self.client = self.app.test_client()
+        self.sale = {
+            "sale-id":1,
+            "product-id":5,
+            "attendant":"mutiso"
+        }
+
+    def test_post_product(self):
+        with self.client:
+            response = self.client.post(
+                '/api/v1/sales', 
+                data = json.dumps(self.sales),
+                headers={'content_type':'application/json'}
+            )
+            # result = json.loads(response.data.decode('utf-8'))
+            # self.assertEqual(response.status_code, 200, result['response'])
+            # self.assertEqual(response, {'hello':'world'})
+            self.assertEqual(response.status_code, 201)
+    
+
+    def test_get_product(self):
+        with self.client:
+            response = self.client.get(
+                '/api/v1/sales',
+                headers={'content_type':'application/json'}
+            )
+            # result = json.loads(response.data.decode('utf-8'))
+            # self.assertEqual(response.status_code, 200, result['Products'])
+            self.assertEqual(response.status_code, 200)
+            # self.assertEqual(response, {'hello':'world'})
+    
+    def test_get_SingleProduct(self):
+        with self.client:
+            response = self.client.get(
+                '/api/v1/sales/1',
+                headers={'content_type':'application/json'}
+            )
+            # result = response.data#json.loads(response.data.decode('utf-8'))
+            self.assertEqual(response.status_code, 200)
+            # self.assertEqual(response.status_code, {'hello':'world'})
+            # self.assertEqual(response.status_code, 200, result['response'])
